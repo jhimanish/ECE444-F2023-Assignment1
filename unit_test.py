38b9d180bdf93c22672576809b5f1cb6142e5cb7
Himanish Jindal <jhimanish@gmail.com>
2023-09-12 00:07:06 -0400

    Adding utils class in unit_test.py with functions

---
+++ unit_test.py
@@ -0,0 +1,17 @@
+class utils:
+    """A simple utils class"""
+
+    def reversed(num):
+        # takes in a number and returns the number reversed
+        # parameter: integer
+        if num >= 0:
+            return int(str(num)[::-1])
+        else:
+            return -1 * int(str(-1 * num)[::-1])
+
+    def formatter(num):
+        # takes in a number and returns the number in base 2 (binary) and base 8 (octal) format
+        # parameter: int
+        num_binary = bin(num)
+        num_octal = oct(num)
+        return num_binary, num_octal
